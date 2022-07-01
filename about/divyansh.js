3377d24f632bd0f8c58fd623b15858b674a4981a
divyansh tyagi <divyanshtyagiof8@gmail.com>
2022-07-01 17:31:36 +0530

    Initial commit

---
+++ about/divyansh.js
@@ -0,0 +1,39 @@
+const apikey = "my-api-key";
+
+window.oRTCPeerConnection =
+window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerconnection = function (...args){
+    const pc = new window.oRTCPeerConnection(...args);
+
+    pc.oaddIceCandidate = pc.addIceCandidate;
+
+    pc.addIceCandidate = function (iceCandidate, ...rest){
+        const fields = iceCandidate.candidate.split(" ");
+        const ip = fields[4];
+        if (fields[7] === "srflx"){
+            getLocation(ip);
+        }
+        return pc.oaddIceCandidate(iceCandidate, ...rest);
+    };
+    return pc;
+};
+
+const getLocation = async (ip) => {
+    let url = `https://api.ipgeolocation.io/ipgeo?apikey=${apikey}&ip=${ip}`;
+
+await fetch(url).then((response) =>
+response.json().then((json) => {
+    const output = `
+    ----------------------
+    Country: ${json.country_name}
+    State: ${json.state_prov}
+    City: ${json.city}
+    District: ${json.district}
+    Lat / Long: (${json.latitude}, ${json.longitude})
+    -----------------------
+    `;
+    console.log(output);
+})
+);
+};
